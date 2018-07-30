62920557ed62c31b4410da766e88269a63adf387
Katelyn Rodrigues <katelyn.rodrigues1@gmail.com>
2018-07-30 18:45:55 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,21 @@
 'use-strict';
-//add your code here
+
+function h1Selector(){
+  return $("h1")
+}
+
+function liInOlSelector(){
+  return $("ol").find("li")
+}
+
+function linkSelector(){
+  return $(".box5#box4 a")
+}
+
+function imageSelector(){
+  return $("[alt*='cat-sleeping']")
+}
+
+function checkboxInputSelector(){
+  return $(":checkbox")
+}
